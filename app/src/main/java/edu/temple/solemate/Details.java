3fce4f0cf4d716e4134a0cbc8157dce9cd95f9fc
sam nguyen <sam.nguyen@temple.edu>
2018-03-17 20:17:58 -0400

    AWS Call. Call is made to server. Server responds with 403 (Forbidden)

--- app/src/main/java/edu/temple/solemate/Details.java
+++ app/src/main/java/edu/temple/solemate/Details.java
@@ -6,9 +6,29 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
 
 /**
  * Created by xxnoa_000 on 2/28/2018.
@@ -17,36 +37,89 @@ import java.io.File;
 public class Details extends Activity {
 
     private ImageView image;
+    private Button recognitionCallButton;
+    private TextView sneakerIDTextView;
 
-    protected void onCreate (Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.savetemp);
 
-        image= (ImageView) findViewById(R.id.imageView3);
+        image = (ImageView) findViewById(R.id.imageView3);
+        recognitionCallButton = (Button) findViewById(R.id.AWSRecogCallButton);
+        sneakerIDTextView = (TextView) findViewById(R.id.sneakerIDTextView);
 
-
-        if(!getIntent().getExtras().getBoolean("boolean")){
-        File imgFile = (File) getIntent().getExtras().get("picture");
+        if (!getIntent().getExtras().getBoolean("boolean")) {
+            File imgFile = (File) getIntent().getExtras().get("picture");
 
             Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
 
             image.setImageBitmap(myBitmap);
             image.setRotation(90);
 
-        }
+            // Instantiate the RequestQueue.
+            final RequestQueue queue = Volley.newRequestQueue(this);
+            final String url = "https://3wpql46dsk.execute-api.us-east-1.amazonaws.com/prod/Recommend_Function/";
 
-        else{
+            // listener for RecognitionCallButton
+            recognitionCallButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // Request a string response from the provided URL using Volley library
+                    // Volley docs: https://developer.android.com/training/volley/request.html
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    sneakerIDTextView.setText("Response: " + response.toString());
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    // TODO: Handle error
+                                    System.out.println(error.networkResponse.headers);
+                                    sneakerIDTextView.setText("That didn't work!");
+
+                                }
+                            }){
+                        @Override
+                        public Map<String, String> getHeaders() throws AuthFailureError {
+                            Map<String, String>  params = new HashMap<String, String>();
+                            params.put("Host", "apigateway.us-east-1.amazonaws.com");
+                            params.put("x-amz-date", getISO8601StringForCurrentDate());
+                            params.put("Authorization", "");
+
+
+                            return params;
+                        }
+                    };
+                    queue.add(jsonObjectRequest);
+                }
+            });
+
+
+        } else {
 
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
 
-            Bitmap myBitmap = BitmapFactory.decodeFile((String)getIntent().getExtras().get("picture2"));
+            Bitmap myBitmap = BitmapFactory.decodeFile((String) getIntent().getExtras().get("picture2"));
 
             image.setImageBitmap(myBitmap);
 
         }
 
 
-       // }
+        // }
+    }
+    // date utils found here https://gist.github.com/kristopherjohnson/6124652
+    public String getISO8601StringForCurrentDate() {
+        Date now = new Date();
+        return getISO8601StringForDate(now);
+    }
+
+    private String getISO8601StringForDate(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
+        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return dateFormat.format(date);
     }
 }
