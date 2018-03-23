81a491b5697f83665d5d8f93f02d7f02e87cc816
NoahTU <strickerx5@gmail.com>
2018-03-22 19:17:48 -0500

    3/22: Pop-up

--- app/src/main/java/edu/temple/solemate/Details.java
+++ app/src/main/java/edu/temple/solemate/Details.java
@@ -5,8 +5,12 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v4.app.ActivityCompat;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.io.File;
 
@@ -18,6 +22,8 @@ public class Details extends Activity {
 
     private ImageView image;
 
+    private TextView header;
+
     protected void onCreate (Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
@@ -25,7 +31,10 @@ public class Details extends Activity {
 
         image= (ImageView) findViewById(R.id.imageView3);
 
+        final BottomSheetDialog mBottomSheetDialog = new BottomSheetDialog(this);
 
+
+        //picture taken
         if(!getIntent().getExtras().getBoolean("boolean")){
         File imgFile = (File) getIntent().getExtras().get("picture");
 
@@ -34,8 +43,28 @@ public class Details extends Activity {
             image.setImageBitmap(myBitmap);
             image.setRotation(90);
 
+            //INSERT CODE FOR AWS ENDPOINT HERE
+
+            final View sheetView = this.getLayoutInflater().inflate(R.layout.pop_up_send, null);
+            mBottomSheetDialog.setContentView(sheetView);
+
+            header= (TextView) sheetView.findViewById(R.id.header);
+            header.setText("Yeezy Powerphase");
+
+
+            mBottomSheetDialog.show();
+            LinearLayout cancel = (LinearLayout) sheetView.findViewById(R.id.fragment_history_bottom_sheet_delete);
+            cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Cancel code here;
+                    mBottomSheetDialog.cancel();
+                }
+            });
+
         }
 
+        //picture from internal storage
         else{
 
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
@@ -45,6 +74,24 @@ public class Details extends Activity {
             System.out.println("Branch test");
 
             image.setImageBitmap(myBitmap);
+
+
+
+            final View sheetView = this.getLayoutInflater().inflate(R.layout.pop_up_send, null);
+            mBottomSheetDialog.setContentView(sheetView);
+
+            header= (TextView) sheetView.findViewById(R.id.header);
+            header.setText("Yeezy Powerphase");
+
+            mBottomSheetDialog.show();
+            LinearLayout cancel = (LinearLayout) sheetView.findViewById(R.id.fragment_history_bottom_sheet_delete);
+            cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Cancel code here;
+                    mBottomSheetDialog.cancel();
+                }
+            });
 
         }
 
