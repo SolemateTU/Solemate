3cd142c13d74f049ad7d70f8a4c44549123090fe
sam nguyen <sam.nguyen@temple.edu>
2018-04-04 13:44:18 -0400

    Display shoe details returned from AWS

--- app/src/main/java/edu/temple/solemate/Details.java
+++ app/src/main/java/edu/temple/solemate/Details.java
@@ -7,19 +7,28 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.v4.app.ActivityCompat;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 
 /**
@@ -29,59 +38,105 @@ import java.io.File;
 public class Details extends Activity {
 
     private ImageView image;
-    private TextView header;
     private TextView dets;
+    private TextView shoeTitle;
+    private ImageView dets_img;
 
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.savetemp);
 
+        Bitmap myBitmap;
         image = (ImageView) findViewById(R.id.imageView3);
 
         // Instantiate the RequestQueue.
         final RequestQueue queue = Volley.newRequestQueue(this);
         // API Gateway endpoint URL
-        final String url = "https://3wpql46dsk.execute-api.us-east-1.amazonaws.com/prod/Recommend_Function/";
+        final String id_url = "https://3wpql46dsk.execute-api.us-east-1.amazonaws.com/prod/identification-function";
 
-        // build request
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        dets.setText(response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            }
-        });
-        // make request to AWS
-        queue.add(stringRequest);
 
         // display image in image view and perform any necessary formatting
         if(!getIntent().getExtras().getBoolean("boolean")){ // picture taken
             File imgFile = (File) getIntent().getExtras().get("picture");
-            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
 
             image.setImageBitmap(myBitmap);
             image.setRotation(90);
         }
         else { //picture from internal storage
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-            Bitmap myBitmap = BitmapFactory.decodeFile((String) getIntent().getExtras().get("picture2"));
+            myBitmap = BitmapFactory.decodeFile((String) getIntent().getExtras().get("picture2"));
 
             image.setImageBitmap(myBitmap);
         }
+
+        //bitmap to byte array
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        String imageString = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+        // build json object
+        JSONObject postparams=new JSONObject();
+        try {
+            postparams.put("img", imageString);
+            postparams.put("userID", "test_id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                id_url, postparams,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            //System.out.println(response);
+                            // display details in pop up
+                            shoeTitle.setText(response.getString("shoeTitle"));
+                            dets.setText(response.getString("shoePrice"));
+                            byte[] decodedString = Base64.decode(response.getString("shoeImage"), Base64.DEFAULT);
+                            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                            dets_img.setImageBitmap(decodedByte);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //Failure Callback
+                    }
+                })
+        {
+            /** Passing some request headers* */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
+        try {
+            System.out.println(jsonObjReq.getHeaders());
+        } catch (AuthFailureError authFailureError) {
+            authFailureError.printStackTrace();
+        }
+
 
         // construct pop-up
         final BottomSheetDialog mBottomSheetDialog = new BottomSheetDialog(this);
         final View sheetView = this.getLayoutInflater().inflate(R.layout.pop_up_send, null);
         mBottomSheetDialog.setContentView(sheetView);
         dets = (TextView) sheetView.findViewById(R.id.details);
+        dets_img = (ImageView) sheetView.findViewById(R.id.detail_image);
+
 
         // label pop-up
-        header = (TextView) sheetView.findViewById(R.id.header);
-        header.setText("Yeezy Powerphase");
+        shoeTitle = (TextView) sheetView.findViewById(R.id.header);
+        shoeTitle.setText("Yeezy Powerphase");
 
         // display pop-up
         mBottomSheetDialog.show();
@@ -93,5 +148,8 @@ public class Details extends Activity {
                 mBottomSheetDialog.cancel();
             }
         });
+
+        // make request to AWS
+        queue.add(jsonObjReq);
     }
 }
