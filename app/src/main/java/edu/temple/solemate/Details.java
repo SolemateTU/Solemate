3c09c6a9ac8ffe2a00785efa0a62855c55b4e746
swin1 <9sYG#989>
2018-04-08 13:53:24 -0400

    Clean & refactor details.java

--- app/src/main/java/edu/temple/solemate/Details.java
+++ app/src/main/java/edu/temple/solemate/Details.java
@@ -50,130 +50,172 @@ import java.util.Map;
 public class Details extends Activity {
 
     private ImageView image;
-    private Button recognitionCallButton;
-    private TextView sneakerIDTextView;
-
-
-    private TextView header;
+    private ImageView dets_img;
     private TextView dets;
     private TextView shoeTitle;
-    private ImageView dets_img;
 
-    protected void onCreate (Bundle savedInstanceState){
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.savetemp);
 
-        Bitmap myBitmap;
-        final String[] tosave = new String[1];
-        final String[] Name = new String[1];
-        final String[] Details = new String[1];
-        final String[] Price = new String[1];
+        Bitmap myBitmap = null;
 
+
+        final BottomSheetDialog mBottomSheetDialog = new BottomSheetDialog(this);
+        final View sheetView = this.getLayoutInflater().inflate(R.layout.pop_up_send, null);
+        mBottomSheetDialog.setContentView(sheetView);
 
         //initiating view objects
         image = (ImageView) findViewById(R.id.imageView3);
+        dets = (TextView) sheetView.findViewById(R.id.details);
+        dets_img = (ImageView) sheetView.findViewById(R.id.detail_image);
+        shoeTitle = (TextView) sheetView.findViewById(R.id.header);
 
-        //final BottomSheetDialog mBottomSheetDialog = new BottomSheetDialog(this);
+        // urls
+        final String url = "https://s7.postimg.org/lavivuo23/Solemate.gif";
+        final String details_url = "https://3wpql46dsk.execute-api.us-east-1.amazonaws.com/prod/identification-function";
+
         // Instantiate the RequestQueue.
         final RequestQueue queue = Volley.newRequestQueue(this);
-        final String url = "https://s7.postimg.org/lavivuo23/Solemate.gif";
-        final String id_url = "https://3wpql46dsk.execute-api.us-east-1.amazonaws.com/prod/identification-function";
 
+        final Intent intent = new Intent(Details.this, Description.class);
+        final boolean[] gtg = {false};
 
-        final Intent intent= new Intent(Details.this, Description.class);
+        // get bitmap and encode as base64
+        myBitmap = display_image(myBitmap);
+        String imageString = bitmapToBase64(myBitmap);
 
+        // build details request
+        JSONObject postParams = new JSONObject();
+        try {
+            postParams.put("img", imageString);
+            postParams.put("userID", "test_id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest detailsRequest = buildDetailsRequest(intent, gtg, postParams, details_url);
+
+        // sample header for pop-up view
+        // can use this code to place in current data for this single instance
+        shoeTitle.setText("Loading");
+        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget((ImageView) sheetView.findViewById(R.id.detail_image));
+        Glide.with(this).load(url).into(imageViewTarget);
+
+        // register cancel and display callbacks
+        LinearLayout cancel = (LinearLayout) sheetView.findViewById(R.id.fragment_history_bottom_sheet_delete);
+        LinearLayout description = (LinearLayout) sheetView.findViewById(R.id.fragment_history_bottom_sheet_edit);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cancel code here;
+                mBottomSheetDialog.cancel();
+            }
+        });
+
+        description.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // go to description;
+                if (gtg[0]) {
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        // display details pop-up
+        mBottomSheetDialog.show();
+
+        // make details request
+        queue.add(detailsRequest);
+    }
+
+    private Bitmap display_image(Bitmap myBitmap) {
         //picture taken
-        if(!getIntent().getExtras().getBoolean("boolean")){
-        File imgFile = (File) getIntent().getExtras().get("picture");
-
-
-
+        if (!getIntent().getExtras().getBoolean("boolean")) {
+            File imgFile = (File) getIntent().getExtras().get("picture");
             myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-
             image.setImageBitmap(myBitmap);
             image.setRotation(90);
-
-
-
         }
-
         //picture from internal storage
-        else{
-
-            if((boolean) getIntent().getExtras().get("boolean2")){
+        else {
+            if ((boolean) getIntent().getExtras().get("boolean2")) {
                 byte[] decodedString = Base64.decode((String) getIntent().getExtras().get("picture2"), Base64.DEFAULT);
                 myBitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
                 image.setImageBitmap(myBitmap);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                myBitmap = BitmapFactory.decodeFile((String) getIntent().getExtras().get("picture2"));
+                image.setImageBitmap(myBitmap);
             }
-            else{
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-            myBitmap = BitmapFactory.decodeFile((String) getIntent().getExtras().get("picture2"));
-            image.setImageBitmap(myBitmap);}
-
         }
+        return myBitmap;
+    }
 
-
-        final boolean[] gtg = {false};
-
-
+    private String bitmapToBase64(Bitmap myBitmap) {
         //bitmap to byte array
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         myBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
         byte[] byteArray = stream.toByteArray();
         String imageString = Base64.encodeToString(byteArray, Base64.DEFAULT);
 
-        // build json object
-        JSONObject postparams=new JSONObject();
-        try {
-            postparams.put("img", imageString);
-            postparams.put("userID", "test_id");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        return imageString;
+    }
 
+    private Bitmap base64ToBitmap(String imageString) {
+        byte[] decodedString = Base64.decode(imageString, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+
+        return decodedByte;
+    }
+
+    private JsonObjectRequest buildDetailsRequest(final Intent intent, final boolean[] gtg, JSONObject postParams, String details_url) {
+        final String[] tosave = new String[1];
+        final String[] Name = new String[1];
+        final String[] Details = new String[1];
+        final String[] Price = new String[1];
 
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
-                id_url, postparams,
+                details_url, postParams,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            //System.out.println(response);
-                            // display details in pop up
+                            // write response to phone storage
                             JSONArray jarray;
-                            if (readFromFile().equals("")){
+                            if (readFromFile().equals("")) {
                                 jarray = new JSONArray();
-                            }
-                            else{
+                            } else {
                                 System.out.println("You sure that's a null output?");
                                 jarray = new JSONArray(readFromFile());
                             }
-                            String test;
-                            shoeTitle.setText(response.getString("shoeTitle"));
-                            Name[0] =response.getString("shoeTitle");
-                            Price[0]=response.getString("shoePrice");
-                            Details[0]=response.getString("shoeDescription");
-                            dets.setText(response.getString("shoePrice"));
-                            byte[] decodedString = Base64.decode(response.getString("shoeImage"), Base64.DEFAULT);
-                            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-                            dets_img.setImageBitmap(decodedByte);
-                            tosave[0] = response.getString("shoeImage");
                             jarray.put(response);
                             writeToFile(jarray.toString());
-                            System.out.println("Title After wait: "+Name[0]);
-                            System.out.println("Current length of array: "+jarray.length());
-                            gtg[0] =true;
 
+                            // display details in pop up
+                            shoeTitle.setText(response.getString("shoeTitle"));
+                            dets.setText(response.getString("shoePrice"));
 
+                            Bitmap imageBitmap = base64ToBitmap(response.getString("shoeImage"));
+                            dets_img.setImageBitmap(imageBitmap);
+
+                            // add details to intent
+                            Name[0] = response.getString("shoeTitle");
+                            Price[0] = response.getString("shoePrice");
+                            Details[0] = response.getString("shoeDescription");
+                            tosave[0] = response.getString("shoeImage");
 
                             intent.putExtra("name", Name[0]);
                             intent.putExtra("price", Price[0]);
                             intent.putExtra("details", Details[0]);
                             intent.putExtra("image", tosave[0]);
 
+                            gtg[0] = true;
 
-
+                            System.out.println("Title After wait: " + Name[0]);
+                            System.out.println("Current length of array: " + jarray.length());
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -184,8 +226,7 @@ public class Details extends Activity {
                     public void onErrorResponse(VolleyError error) {
                         //Failure Callback
                     }
-                })
-        {
+                }) {
             /** Passing some request headers* */
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
@@ -194,71 +235,19 @@ public class Details extends Activity {
                 return headers;
             }
         };
-        try {
-            System.out.println(jsonObjReq.getHeaders());
-        } catch (AuthFailureError authFailureError) {
-            authFailureError.printStackTrace();
-        }
 
-
-        final BottomSheetDialog mBottomSheetDialog = new BottomSheetDialog(this);
-        final View sheetView = this.getLayoutInflater().inflate(R.layout.pop_up_send, null);
-        mBottomSheetDialog.setContentView(sheetView);
-        dets = (TextView) sheetView.findViewById(R.id.details);
-        dets_img = (ImageView) sheetView.findViewById(R.id.detail_image);
-
-        //sample header, can use this code to place in current data for this single instance
-        shoeTitle = (TextView) sheetView.findViewById(R.id.header);
-        header= (TextView) sheetView.findViewById(R.id.header);
-        shoeTitle.setText("Loading");
-
-
-        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget((ImageView) sheetView.findViewById(R.id.detail_image));
-        Glide.with(this).load(url).into(imageViewTarget);
-
-
-        mBottomSheetDialog.show();
-        LinearLayout cancel = (LinearLayout) sheetView.findViewById(R.id.fragment_history_bottom_sheet_delete);
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Cancel code here;
-                mBottomSheetDialog.cancel();
-            }
-        });
-        LinearLayout description = (LinearLayout) sheetView.findViewById(R.id.fragment_history_bottom_sheet_edit);
-        //if (Name[0]!="null"){
-        description.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // go to description;
-                //startActivity(new Intent(Details.this, Description.class));
-                //System.out.println("Is it Null? "+Name[0]);
-
-                if (gtg[0]){
-                startActivity(intent);}
-
-            }
-        });//}
-
-        System.out.println("The Request 2: "+jsonObjReq);
-
-        queue.add(jsonObjReq);
-        System.out.println("Title: "+Name[0]);
-        System.out.println("Description: "+Details[0]);
-        System.out.println("Price: "+Price[0]);
+        return jsonObjReq;
     }
 
     private void writeToFile(String data) {
         try {
-            System.out.println("DATA BEING WROTE: "+data);
+            System.out.println("DATA BEING WROTE: " + data);
             File mFolder = new File("/data/user/0/edu.temple.solemate/files");
             File imgFile = new File(mFolder.getAbsolutePath() + "/config.txt");
 
             FileOutputStream fOut = new FileOutputStream(imgFile, false);/// may have to delete false
             OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
             myOutWriter.append(data);
-            //myOutWriter.write(data);
 
             myOutWriter.close();
 
@@ -270,7 +259,6 @@ public class Details extends Activity {
         }
     }
 
-
     public String readFromFile() {
 
         String ret = "";
@@ -278,27 +266,26 @@ public class Details extends Activity {
         try {
             InputStream inputStream = getApplicationContext().openFileInput("config.txt");
 
-            if ( inputStream != null ) {
+            if (inputStream != null) {
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                 String receiveString = "";
                 StringBuilder stringBuilder = new StringBuilder();
 
-                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                while ((receiveString = bufferedReader.readLine()) != null) {
                     stringBuilder.append(receiveString);
                 }
 
                 inputStream.close();
                 ret = stringBuilder.toString();
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             Log.e("login activity", "File not found: " + e.toString());
         } catch (IOException e) {
             Log.e("login activity", "Can not read file: " + e.toString());
         }
 
-        System.out.println("DATA BEING READ: "+ret);
+        System.out.println("DATA BEING READ: " + ret);
         return ret;
     }
 
